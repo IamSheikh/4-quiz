15f648f03d7f5010cc810542ee34b7bbe334f4b9
babydmg <126503300+babydmg@users.noreply.github.com>
2024-11-03 15:08:13 +0500

    some more

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import { IResultProps } from "../types";
+import { IResultProps } from '../types';
 
 const Result: React.FC<IResultProps> = ({ score, total, restartQuiz }) => {
   const percentage = ((score / total) * 100).toFixed(2);
 
   return (
-    <div className="text-center">
-      <h2 className="text-2xl font-bold mb-4 mt-4">Quiz Completed!</h2>
-      <p className="text-lg mb-2">
-        You scored <span className="font-semibold">{score}</span> out of{" "}
-        <span className="font-semibold">{total}</span>
+    <div className='text-center'>
+      <h2 className='text-2xl font-bold mb-4 mt-4'>Quiz Completed!</h2>
+      <p className='text-lg mb-2'>
+        You scored <span className='font-semibold'>{score}</span> out of{' '}
+        <span className='font-semibold'>{total}</span>
       </p>
-      <p className="text-lg mb-6">Your Score: {percentage}%</p>
+      <p className='text-lg mb-6'>Your Score: {percentage}%</p>
       <button
         onClick={restartQuiz}
-        className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition"
+        className='bg-blue-800 text-white py-2 px-4 rounded hover:bg-blue-900 transition'
       >
         Restart Quiz
       </button>
